40837c25c4472ba7b3a81b1a1ac4a8b5b23426ae
Hadj Aissa <hadjaissasaid@gmail.com>
2026-08-18 23:40:59 +0300

    insert_data.sql file added.

---
+++ sql/03_insert_data.sql.sql
@@ -0,0 +1,18 @@
+INSERT INTO customers (
+    first_name,
+    last_name,
+    email,
+    country,
+    city
+)
+VALUES
+('John', 'Smith', 'john@example.com', 'USA', 'New York'),
+('Anna', 'Miller', 'anna@example.com', 'Germany', 'Berlin'),
+('Marco', 'Rossi', 'marco@example.com', 'Italy', 'Rome'),
+('Emma', 'Wilson', 'emma@example.com', 'UK', 'London'),
+('Lucas', 'Silva', 'lucas@example.com', 'Brazil', 'São Paulo'),
+('Sophie', 'Brown', 'sophie@example.com', 'France', 'Paris'),
+('Daniel', 'Garcia', 'daniel@example.com', 'Spain', 'Madrid'),
+('Olivia', 'Taylor', 'olivia@example.com', 'USA', 'Chicago'),
+('Liam', 'Anderson', 'liam@example.com', 'Canada', 'Toronto'),
+('Mia', 'Martin', 'mia@example.com', 'France', 'Lyon');
